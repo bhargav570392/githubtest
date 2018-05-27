49cf22478b4561c1941bdd7db2b51871949696d0
shoeb12345 <shoebmohammed.nyc@gmail.com>
2018-05-27 19:42:00 +0000

    FL-SHOAIB: Second Commit

--- server/src/main/java/com/javatpoint/controller/ContactController.java
+++ server/src/main/java/com/javatpoint/controller/ContactController.java
@@ -12,15 +12,10 @@ import com.javatpoint.form.Contact;
 public class ContactController {  
     @RequestMapping(value = "/addContact", method = RequestMethod.POST)  
     public String addContact(@ModelAttribute("contact") Contact contact, BindingResult result) {  
-<<<<<<< HEAD
-<<<<<<< HEAD
         // Sandy has written the code here to add contact  
-=======
         //write the code here to add contact  
->>>>>>> a845a2e... FL-TEJA: Add java package.
-=======
+        //write the code here to add contact 
         //write the code here to add contact  
->>>>>>> a845a2e... FL-TEJA: Add java package.
     	String message = contact.toString();
         return "redirect:contact.html"+message;  
     }  
@@ -29,12 +24,9 @@ public class ContactController {
     public ModelAndView showContacts() {  
         return new ModelAndView("contact", "command", new Contact());  
     }  
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 }  
-=======
+
 }  
->>>>>>> a845a2e... FL-TEJA: Add java package.
-=======
-}  
->>>>>>> a845a2e... FL-TEJA: Add java package.
+}  }  
