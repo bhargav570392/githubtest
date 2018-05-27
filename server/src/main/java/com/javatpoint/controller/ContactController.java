d57cd370341e22170dfd506fe8e55dfd80c386d6
dileep53 <itsurdilip@gmail.com>
2018-05-27 15:52:56 -0400

    FL-DILEEP: wishing everyone

--- server/src/main/java/com/javatpoint/controller/ContactController.java
+++ server/src/main/java/com/javatpoint/controller/ContactController.java
@@ -12,15 +12,6 @@ import com.javatpoint.form.Contact;
 public class ContactController {  
     @RequestMapping(value = "/addContact", method = RequestMethod.POST)  
     public String addContact(@ModelAttribute("contact") Contact contact, BindingResult result) {  
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // Sandy has written the code here to add contact  
-=======
-        //write the code here to add contact  
->>>>>>> a845a2e... FL-TEJA: Add java package.
-=======
-        //write the code here to add contact  
->>>>>>> a845a2e... FL-TEJA: Add java package.
     	String message = contact.toString();
         return "redirect:contact.html"+message;  
     }  
@@ -28,13 +19,8 @@ public class ContactController {
     @RequestMapping("/contact")  
     public ModelAndView showContacts() {  
         return new ModelAndView("contact", "command", new Contact());  
-    }  
-<<<<<<< HEAD
-<<<<<<< HEAD
+    } Hello everyone
+
+    //wishing everyone
 }  
-=======
-}  
->>>>>>> a845a2e... FL-TEJA: Add java package.
-=======
-}  
->>>>>>> a845a2e... FL-TEJA: Add java package.
+
