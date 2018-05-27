243e7fcf97f90dd39f558c6cb0d5948ef516b90c
Mazin <mazin_v8@yahoo.com>
2018-05-27 16:03:50 -0500

    First edit

--- server/src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ server/src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -2,7 +2,10 @@ package com.javatpoint.controller;
 import org.springframework.stereotype.Controller;  
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.servlet.ModelAndView;  
-@Controller  
+@Controller //This is the second time we are editing this file so that we get better at this
+//This isn't the last time we are doing this task. 
+//We have six sub tasks in this main task.
+//All six are important parts of this task
 public class HelloWorldController {  
     @RequestMapping("/hello")  
     public ModelAndView helloWorld() {  
